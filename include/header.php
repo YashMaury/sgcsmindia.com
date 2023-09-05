26fc26bfe63a3791b9d593a19786a3fc736acc27
TANUGICT <tanugict@gmail.com>
2023-09-05 08:18:45 +0530

    header.php

--- include/header.php
+++ include/header.php
@@ -1,16 +1,10 @@
 <!DOCTYPE html>
 
 <html dir="ltr" lang="en">
-
-
-
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title></title>
-
     <link href="https://sgcsmindia.org/templates/assets/style.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
@@ -21,29 +15,19 @@
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"> -->
     <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css" rel="stylesheet"> -->
 
-
     <script src="assets/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 
     <script type="text/javascript"
         src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
-
     <script>
 
         function googleTranslateElementInit() {
-
             new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
 
         }
-
     </script>
-
-    <script src="https://sgcsmindia.org/templates/assets/css/engine1/jquery.js"></script>
-
-    <link href="https://sgcsmindia.org/templates/assets/css/engine1/style.css" rel="stylesheet" type="text/css" />
-
-
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
 
@@ -53,11 +37,7 @@
         href="https://fonts.googleapis.com/css2?family=Lora:wght@500&family=Roboto:ital,wght@0,400;0,500;1,400&display=swap"
         rel="stylesheet">
 
-
-
 </head>
-
-
 
 <body>
 
@@ -72,14 +52,14 @@
 
             <div class="col-sm-">
                 <ul>
-                    <li><a href="https://sgcsmindia.org/centerpanel/center-login" target="_blank">Center Login</a></li>
+                    <li><a href="center-login.php" target="_blank">Center Login</a></li>
                     <li><a target="_blank" href="https://tinyurl.com/24am9w39"><i class="bi bi-facebook"></i></a></li>
                     <li><a target="_blank" href="https://twitter.com/sgcsmindia1"><i class="bi bi-twitter"></i></a></li>
                     <li><a target="_blank" href="https://www.instagram.com/sgcsmskills/"><i
                                 class="bi bi-instagram"></i></a></li>
                     <li><a target="_blank" href="https://tinyurl.com/2hskutzh"><i class="bi bi-linkedin"></i></a></li>
                     <li><a target="_blank" href="https://tinyurl.com/2p8radb5"><i class="bi bi-youtube"></i></a></li>
-                    <li><a target="_blank" href="https://sgcsmindia.org/admin/login" class="login-top-link">Admin Login
+                    <li><a target="_blank" href="admin/login.php" class="login-top-link">Admin Login
                             <i style="background:#7297ff; color:#000;" class="bi bi-caret-right-square-fill"></i></a>
                     </li>
                 </ul>
@@ -88,14 +68,12 @@
         </div>
     </nav>
 
-
-
     <header class="navbar navbar-dark px-5" id="header">
         <div class="container-fluid">
 
             <div class="col-sm-6">
                 <div class="logo">
-                    <a href="https://sgcsmindia.org/">
+                    <a href="index.php">
                         <img class="img-fluid" src="https://sgcsmindia.org/uploads/images/logo.png">
                     </a>
                 </div>
@@ -128,7 +106,7 @@
                     <ul class="navbar-nav mr-auto">
                         
                         <li class="nav-item">
-                            <a class="nav-link" href="https://sgcsmindia.org/">HOME</a>
+                            <a class="nav-link" href="index.php">HOME</a>
                         </li>
 
                         <li class="nav-item dropdown">
@@ -142,21 +120,21 @@
 
                             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/about">About Us</a>
+                                <a class="dropdown-item" href="about.php">About Us</a>
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/mission-vision">Mission &
+                                <a class="dropdown-item" href="mission-vision.php">Mission &
                                     Vision</a>
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/our-dream">Our Dream</a>
+                                <a class="dropdown-item" href="our-dream.php">Our Dream</a>
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/advantages">Advantages</a>
+                                <a class="dropdown-item" href="advantages.php">Advantages</a>
 
 
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/director-message">Director's
+                                <a class="dropdown-item" href="director-message.php">Director's
                                     Message</a>
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/our-team">Our Management Team</a>
+                                <a class="dropdown-item" href="our-team.php">Our Management Team</a>
 
                             </div>
 
@@ -180,32 +158,31 @@
 
 
                                 <a class="dropdown-item"
-                                    href="https://sgcsmindia.org/course?type=COMPUTER_COURSES">COMPUTER COURSES</a>
+                                    href="course?type=COMPUTER_COURSES.php">COMPUTER COURSES</a>
 
                                 <a class="dropdown-item"
-                                    href="https://sgcsmindia.org/course?type=TEACHER_TRAINING_COURSE">TEACHER TRAINING
-                                    COURSE</a>
+                                    href="course?type=TEACHER_TRAINING_COURSE.php">TEACHER TRAINING COURSE</a>
+                                    
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/course?type=NIELIT_COURSES">NIELIT
-                                    COURSES</a>
-
-                                <a class="dropdown-item"
-                                    href="https://sgcsmindia.org/course?type=BOUTIQUE_COURSES">BOUTIQUE COURSES</a>
+                                <a class="dropdown-item" href="course?type=NIELIT_COURSES.php">NIELIT COURSES</a>
+                                    
 
                                 <a class="dropdown-item"
-                                    href="https://sgcsmindia.org/course?type=BEAUTICIAN_COURSES">BEAUTICIAN COURSES </a>
-
-                                <a class="dropdown-item" href="https://sgcsmindia.org/course?type=UG_PG_COURSE">UG & PG
-                                    COURSE</a>
-
-                                <a class="dropdown-item" href="https://sgcsmindia.org/course?type=YOGA_COURSES">YOGA
-                                    COURSES</a>
-
-                                <a class="dropdown-item" href="https://sgcsmindia.org/course?type=NDLM_COURSES">NDLM
-                                    COURSES</a>
+                                    href="course?type=BOUTIQUE_COURSES.php">BOUTIQUE COURSES</a>
 
                                 <a class="dropdown-item"
-                                    href="https://sgcsmindia.org/course?type=ADVANCE_COURSES">ADVANCE COURSES</a>
+                                    href="course?type=BEAUTICIAN_COURSES.php">BEAUTICIAN COURSES </a>
+
+                                <a class="dropdown-item" href="course?type=UG_PG_COURSE.php">UG & PG COURSEs </a>                                    
+
+                                <a class="dropdown-item" href="course?type=YOGA_COURSES.php">YOGA  COURSES</a>
+                                   
+
+                                <a class="dropdown-item" href="course?type=NDLM_COURSES.php">NDLM COURSES</a>
+                                    
+
+                                <a class="dropdown-item"
+                                    href="course?type=ADVANCE_COURSES.php">ADVANCE COURSES</a>
 
 
 
@@ -232,19 +209,19 @@
 
                             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/download">Downloads</a>
+                                <a class="dropdown-item" href="download.php">Downloads</a>
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/appriciation-letter">Appreciation
-                                    Letters</a>
+                                <a class="dropdown-item" href="appriciation-letter.php">Appreciation Letters</a>
+                                    
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/linkage-affiliation">Linkage &
-                                    Affiliation</a>
+                                <a class="dropdown-item" href="linkage-affiliation.php">Linkage & Affiliation</a>
+                                    
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/how-to-get-affiliaction">How to
-                                    Get Affiliation</a>
+                                <a class="dropdown-item" href="how-to-get-affiliaction.php">How to  Get Affiliation</a>
+                                   
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/authorized-studycenter">Authorized
-                                    Study Center</a>
+                                <a class="dropdown-item" href="authorized-studycenter.php">Authorized Study Center</a>
+                                    
 
                             </div>
 
@@ -267,14 +244,14 @@
 
                             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/student-details">Student
-                                    Information</a>
+                                <a class="dropdown-item" href="student-details.php">Student Information</a>
+                                    
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/online-result">Online Result</a>
+                                <a class="dropdown-item" href="online-result.php">Online Result</a>
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/studentlogin">Student Login</a>
+                                <a class="dropdown-item" href="studentlogin.php">Student Login</a>
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/exam-login">Exam Login</a>
+                                <a class="dropdown-item" href="exam-login.php">Exam Login</a>
 
                             </div>
 
@@ -299,17 +276,17 @@
 
                                 <!---<a class="dropdown-item" href="">Registration Process</a>--->
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/centerpanel/center-login"
-                                    target="_blank">ASC Login</a>
+                                <a class="dropdown-item" href="centerpanel/center-login.php"target="_blank">ASC Login</a>
+                                    
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/bank-details">Our Account (Bank
-                                    Details)</a>
+                                <a class="dropdown-item" href="bank-details.php">Our Account (Bank  Details)</a>
+                                   
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/business-support-system">Business
-                                    Support System</a>
+                                <a class="dropdown-item" href="business-support-system.php">Business Support System</a>
+                                    
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/rules-regulation">SGCSM Rules &
-                                    regulation</a>
+                                <a class="dropdown-item" href="rules-regulation.php">SGCSM Rules &  regulation</a>
+                                   
 
                             </div>
 
@@ -334,14 +311,14 @@
 
                             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/franchise-online-form">Online
-                                    apply for Franchise</a>
+                                <a class="dropdown-item" href="franchise-online-form.php">Online apply for Franchise</a>
+                                    
 
                                 <a target="_blank" class="dropdown-item"
-                                    href="uploads/pdf/2018-12-09-11-03-20--5c0cf678528f4-_58d607d26ca53.pdf">Off Line
-                                    Affiliation Form</a>
+                                    href="uploads/pdf/2018-12-09-11-03-20--5c0cf678528f4-_58d607d26ca53.pdf">Off Line Affiliation Form</a>
+                                    
 
-                                <a class="dropdown-item" href="https://sgcsmindia.org/public-notice">Public Notice</a>
+                                <a class="dropdown-item" href="public-notice.php">Public Notice</a>
 
                             </div>
 
@@ -362,16 +339,16 @@
 
                                 <a class="dropdown-item" href="our-publication.php">Our Publication</a>
                                 <a class="dropdown-item" href="our-gallery.php">Gallery</a>
-                                <a class="dropdown-item" href="https://sgcsmindia.org/placement-cell">Placement Cell</a>
-                                <a class="dropdown-item" href="https://sgcsmindia.org/news-events">News & Events</a>
-                                <a class="dropdown-item" href="https://sgcsmindia.org/media-coverage">Media Coverage</a>
-                                <a class="dropdown-item" href="https://sgcsmindia.org/study-material">Our Study Materials</a>
+                                <a class="dropdown-item" href="placement-cell.php">Placement Cell</a>
+                                <a class="dropdown-item" href="news-events.php">News & Events</a>
+                                <a class="dropdown-item" href="media-coverage.php">Media Coverage</a>
+                                <a class="dropdown-item" href="study-material.php">Our Study Materials</a>
 
                             </div>
 
                         </li>
 
-                        <li class="nav-item"><a class="nav-link" href="https://sgcsmindia.org/contact">CONTACT US</a>
+                        <li class="nav-item"><a class="nav-link" href="contact.php">CONTACT US</a>
                         </li>
 
 
