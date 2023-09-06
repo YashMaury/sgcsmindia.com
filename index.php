be7acdc3c8b4d8ddec74bf70d7b2a8dfecb16e76
YashMaury <yasmaurya2000@gmail.com>
2023-09-06 08:08:21 +0530

    upd

--- index.php
+++ index.php
@@ -1,38 +1,123 @@
-<?php include "include/header.php" ;?>
+<?php include "include/header.php"; ?>
 
+<section>
 
-<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="assets/images/beauty2.webp" class="d-block w-100" alt="...">
+  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="assets/images/beauty2.webp" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/images/beauty1.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/images/computer1.jpeg" class="d-block w-100" alt="...">
+      </div>
     </div>
-    <div class="carousel-item">
-      <img src="assets/images/beauty1.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="assets/images/computer1.jpeg" class="d-block w-100" alt="...">
-    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+      data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+      data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
 
 
-<div style="background-color:aqua;">
+  <div style="background-color:aqua;">
     <marquee behavior="" direction="">
-    "संजय गाँधी कम्प्यूटर साक्षरता मिशन" की केवल एक ही वेबसाइट है इसलिए फर्जी और झूठे लोगों के जालसाजी में न आयें, संजय गाँधी कम्प्यूटर साक्षरता मिशन किसी भी प्रकार कैश का लेनदेन नहीं करता है, किसी भी व्यक्ति को कैश न दें, कोई भी लेनदेन संस्था के एकाउंट के माध्यम से ही करें I
+      "संजय गाँधी कम्प्यूटर साक्षरता मिशन" की केवल एक ही वेबसाइट है इसलिए फर्जी और झूठे लोगों के जालसाजी में न आयें,
+      संजय गाँधी कम्प्यूटर साक्षरता मिशन किसी भी प्रकार कैश का लेनदेन नहीं करता है, किसी भी व्यक्ति को कैश न दें, कोई भी
+      लेनदेन संस्था के एकाउंट के माध्यम से ही करें I
     </marquee>
-</div>
+  </div>
+
+</section>
+
+<section class="container">
+
+  <center class="mt-4">
+    <img src="assets/images/text/welcome.jpg" alt="">
+  </center>
+  <br />
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae necessitatibus, porro earum obcaecati sed dolor
+    eligendi libero placeat amet deleniti, et in quas nam, error consequatur modi odit ea at?</p>
+
+  <div class="d-flex">
+    <img class="w-25" src="assets/images/btn/center-login.jpg" alt="">
+    <img class="w-25" src="assets/images/btn/exam-login.jpg" alt="">
+    <img class="w-25" src="assets/images/btn/student-login.jpg" alt="">
+    <img class="w-25" src="assets/images/btn/form.jpg" alt="">
+  </div>
+
+</section>
+
+<section class="container">
+
+  <center class="mt-4">
+    <img src="assets/images/text/courses.jpg" alt="">
+  </center>
+
+  <div class="d-flex mt-4">
+    <div class="">
+      <img class="w-100 img-fluid" src="assets/images/gict/1.png" alt="">
+      <img class="w-100 img-fluid" src="assets/images/gict/2.png" alt="">
+      <img class="w-100 img-fluid" src="assets/images/gict/3.png" alt="">
+      <img class="w-100 img-fluid" src="assets/images/gict/4.png" alt="">
+      <img class="w-100 img-fluid" src="assets/images/gict/5.png" alt="">
+    </div>
+    <img class="img-fluid w-50" src="assets/images/gict/side-1.jfif" alt="">
+  </div>
+
+</section>
+
+<section class="container">
+
+  <center class="mt-4">
+    <img src="assets/images/text/acheivments.jpg" alt="">
+  </center>
+
+  <div class="d-flex gap-2 mt-4">
+    <img class="w-25" src="assets/images/b1.png" alt="">
+    <img class="w-25" src="assets/images/b2.png" alt="">
+    <img class="w-25" src="assets/images/b1.png" alt="">
+    <img class="w-25" src="assets/images/b2.png" alt="">
+  </div>
+</section>
+
+<section class="container">
+
+  <center class="mt-4">
+    <img src="assets/images/text/course.jpg" alt="">
+  </center>
+
+  <div class="d-flex gap-2 mt-4">
+    <img class="w-25" src="assets/images/b1.png" alt="">
+    <img class="w-25" src="assets/images/b2.png" alt="">
+    <img class="w-25" src="assets/images/b1.png" alt="">
+    <img class="w-25" src="assets/images/b2.png" alt="">
+  </div>
+</section>
 
 
+<section class="mt-4" style="background-color:#000">
+  <div class="d-flex container">
+
+    <div class="text-white container py-5">
+      <h4 class="text-white h1">BE AWARE OF FAKE GICT</h4>
+      <h4 class="">Fake organization using the name of GICT illegally</h4>
+
+      <button class="btn btn-secondary">Information About Fake GICT</button>
+    </div>
+    <div class="img w-25">
+      <img class="w-100" src="assets/images/btn/alert.png" alt="">
+    </div>
+
+  </div>
+</section>
 
 
-
-
-<?php include "include/footer.php" ;?>
+<?php include "include/footer.php"; ?>
