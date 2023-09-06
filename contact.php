b2f5ff4b1bf39c75bf408cb7c2b9b8f90109b4a3
YashMaury <yasmaurya2000@gmail.com>
2023-09-06 21:43:15 +0530

    upd

---
+++ contact.php
@@ -0,0 +1,45 @@
+<?php include "include/header.php"; ?>
+
+<div class="achivements" style="background: url(assets/images/slide/breadcrumb.jpg);padding: 50px;">
+    <div class="sec-title mb-3 text-center">
+        <h2 class="mb-0" style="color:#fff;">CONTACT US</h2>
+        <p style="color:#fff;">Home / Contact Us</p>
+    </div>
+    <div class="container">
+        <div class="row">
+        </div>
+    </div>
+</div>
+
+<div class="container content">
+    <div class="sec-title mb-3 text-center">
+        <h2 class="mb-0">CONTACT US</h2>
+    </div>
+    <div class="mb-4" align="justify">
+
+        <div class="form-boxs">
+            <div class="row">
+                <div class="col-sm-6">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1797.8809799333487!2d82.21749166931154!3d25.679184438341206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1694015614812!5m2!1sen!2sin"
+                        width="550" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+
+                <div class="col-sm-6">
+                    <p class="main-title">Head Office</p>
+                    <p class="main-content">Jaunpur</p>
+                    <p class="main-content"> User : 999999999 , User : 999999999</p>
+                    <p></p>
+                    <p class="main-title">Our Coordinators</p>
+                    <p class="main-content">UP State Coordinator : 99999999</p>
+                    <p class="main-content">Email Id: info@gictindia.org, gicteducomputer@gmail.com</p>
+                    <p class="main-content">Visit us: www.gictcomputer.org</p>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "include/footer.php"; ?>
