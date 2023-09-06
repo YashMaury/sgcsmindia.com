6a05eadb1764a15406a8ebb034d9cd14988cc729
YashMaury <yasmaurya2000@gmail.com>
2023-09-06 22:10:44 +0530

    upd

--- appriciation-letter.php
+++ appriciation-letter.php
@@ -1,2 +1,40 @@
 <?php include "include/header.php"; ?>
+
+
+<div class="achivements" style="background: url(assets/images/slide/breadcrumb.jpg);padding: 50px;">
+    <div class="sec-title mb-3 text-center">
+        <h2 class="mb-0" style="color:#fff;">APPRECIATION LETTER</h2>
+        <p style="color:#fff;">Home / Appreciation Letter</p>
+    </div>
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</div>
+
+<div class="container content">
+    <div class="sec-title mb-3 text-center">
+        <h2 class="mb-0">Appreciation Letter</h2>
+    </div>
+    <div class="mb-4" align="justify">
+        <div class="row">
+
+            <div class="col-sm-4">
+                <div class="card text-center">
+                    <i class="bi bi-file-earmark-arrow-down-fill card-img-top" style="font-size: 100px;"></i>
+                    PDF I
+                    <div class="card-body">
+                        <a href="" class="btn btn-primary" download><i class="bi bi-download"></i> Download</a>
+                        <p style="font-size: 10px;">1.pdf</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
 <?php include "include/footer.php"; ?>
--- download.php
+++ download.php
@@ -1,2 +1,62 @@
 <?php include "include/header.php"; ?>
+
+
+<div class="achivements" style="background: url(assets/images/slide/breadcrumb.jpg);padding: 50px;">
+    <div class="sec-title mb-3 text-center">
+        <h2 class="mb-0" style="color:#fff;">DOWNLOADS</h2>
+        <p style="color:#fff;">Home / Downloads</p>
+    </div>
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</div>
+
+<div class="container content">
+    <div class="sec-title mb-3 text-center">
+        <h2 class="mb-0">DOWNLOADS</h2>
+    </div>
+    <div class="mb-4" align="justify">
+        <div class="row">
+
+            <div class="col-sm-4">
+                <div class="card text-center">
+                    <i class="bi bi-file-earmark-arrow-down-fill card-img-top" style="font-size: 100px;"></i>
+                    PDF I
+                    <div class="card-body">
+                        <a href="" class="btn btn-primary" download><i class="bi bi-download"></i> Download</a>
+                        <p style="font-size: 10px;">1.pdf</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="card text-center">
+                    <i class="bi bi-file-earmark-arrow-down-fill card-img-top" style="font-size: 100px;"></i>
+                    PDF II
+                    <div class="card-body">
+                        <a href="" class="btn btn-primary" download><i class="bi bi-download"></i> Download</a>
+                        <p style="font-size: 10px;">2.pdf</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="card text-center">
+                    <i class="bi bi-file-earmark-arrow-down-fill card-img-top" style="font-size: 100px;"></i>
+                    PDF III
+                    <div class="card-body">
+                        <a href="" class="btn btn-primary" download><i class="bi bi-download"></i> Download</a>
+                        <p style="font-size: 10px;">3.pdf</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
 <?php include "include/footer.php"; ?>
