a2a1866e5db778aceb7c367d8deb9e305d7ce1b1
YashMaury <yasmaurya2000@gmail.com>
2023-09-06 22:14:22 +0530

    upd

--- linkage-affiliation.php
+++ linkage-affiliation.php
@@ -1,2 +1,54 @@
 <?php include "include/header.php"; ?>
+
+
+<div class="achivements" style="background: url(assets/images/slide/breadcrumb.jpg);padding: 50px;">
+    <div class="sec-title mb-3 text-center">
+        <h2 class="mb-0" style="color:#fff;text-transform: uppercase;">Linkage & Affiliation</h2>
+        <p style="color:#fff;">Home / Linkage & Affiliation</p>
+    </div>
+    <div class="container">
+        <div class="row">
+        </div>
+    </div>
+</div>
+
+<div class="container content">
+    <div class="row" style="text-transform: uppercase;">
+
+        <div class="col-sm-8 ccc">
+            <h3>Linkage & Affiliation</h3>
+            <div>
+                <i class="bi bi-chevron-double-right"></i> GICT COMPUTER SAKSHARTA MISSION (GICTCSM)
+            </div>
+            <div>
+                <i class="bi bi-chevron-double-right"></i> GICT WOMEN TRAINING INSTITUTE (GICTWTI)
+            </div>
+            <div>
+                <i class="bi bi-chevron-double-right"></i> GICT YOG SANSTHAN (GICTYS)
+            </div>
+            <div>
+                <i class="bi bi-chevron-double-right"></i> GICT ADVANCE TRAINING INSTITUTE (GICTATI)
+            </div>
+            <div>
+                <i class="bi bi-chevron-double-right"></i> GICT EARLY CHILDHOOD CARE & EDUCATION (GICTECCE)
+            </div>
+            <div>
+                <i class="bi bi-chevron-double-right"></i> GICT INSTITUTE OF SOLAR ENERGY (GICTISE)
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <img class="img-fluid img-thumbnail" src="assets/images/child2.webp">
+        </div>
+
+    </div>
+
+</div>
+<style>
+    .ccc {
+        line-height: 35px;
+    }
+</style>
+
+
 <?php include "include/footer.php"; ?>
