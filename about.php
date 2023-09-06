feaa4415c42f52ae7202744d8347de5b57b195e3
YashMaury <yasmaurya2000@gmail.com>
2023-09-06 22:18:15 +0530

    upd

--- about.php
+++ about.php
@@ -1,31 +1,77 @@
 <?php include "include/header.php"; ?>
 
 
-<div class="pos_cen">
-    <img class="img_deg" src="assest/images/mission.jpg">
-    <p style="color: red;">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.<br>
-         Ipsa debitis officia saepe quaerat eveniet, qui itaque <br>
-         delectus ipsum esse repudiandae sint, accusamus doloribus voluptas<br> 
-        numquam illum similique enim. Vitae, rem!<br>
-    </p>
+<div class="achivements" style="background: url(assets/images/slide/breadcrumb.jpg);padding: 50px;">
+    <div class="sec-title mb-3 text-center">
+        <h2 class="mb-0" style="color:#fff;">ABOUT US</h2>
+        <p style="color:#fff;">Home / About Us</p>
     </div>
-    <div class="pos_cen">
-    <img class="img_deg" src="assest/images/mission.jpg">
-    <p style="color: red;">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.<br>
-         Ipsa debitis officia saepe quaerat eveniet, qui itaque <br>
-         delectus ipsum esse repudiandae sint, accusamus doloribus voluptas<br> 
-        numquam illum similique enim. Vitae, rem!<br>
-    </p>
+    <div class="container">
+        <div class="row">
+
+        </div>
     </div>
-    <div class="pos_cen">
-    <img class="img_deg" src="assets/images/mission.jpg">
-    <p style="color: red;">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.<br>
-         Ipsa debitis officia saepe quaerat eveniet, qui itaque <br>
-         delectus ipsum esse repudiandae sint, accusamus doloribus voluptas<br> 
-        numquam illum similique enim. Vitae, rem!<br>
-    </p>
+</div>
+
+<div class="container content">
+    <div class="row">
+
+        <div class="col-sm-8">
+            <h3>About Us</h3>
+            <div>
+                Sanjay Gandhi Computer Saksharta Mission Trust has been registered under the Public Trust Act 1882,
+                (Reg.No.2327) from Govt. Of India N.C.T., Delhi, working in different fields of Programme & Commercial
+                Training Organization is also certified by ISO 9001: 2015 Org. Through the Govt. is taking major steps
+                in influencing the computer education in the country but the participation of NGO's in spreading this
+                vital utility technical education can't be denied. After a through survey it was found that there is a
+                lot of awareness of computers even in the remotest of areas but the general public can't be equipped
+                with the art firstly due to unaffordable charges laid down by big private computer centers and also that
+                the general public have some misconception in their mind about learning computers.
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <img class="img-fluid img-thumbnail" src="/c04e1242-7cfa-4a99-8123-f80511541b3a.jfif" />
+        </div>
+
     </div>
+
+
+    <div class="row">
+
+        <div class="col-sm-4">
+            <img class="img-fluid img-thumbnail" src="/dbda31b8-1d68-470f-b55d-b3729ab77b44.jfif" />
+        </div>
+        <div class="col-sm-8">
+            <h3>Our Mission</h3>
+            <div>
+                Today, the computer is indispensable, and its presence has become very important and necessary in our
+                daily life, and it has become easier for us to do many operations and activities.some big Institution
+                run their computer programme in higher charges. Due to which the middle class families of our societies
+                cannot afford the load of their charges. Our aim is to make everyone Computer Literacy to every nook and
+                corner of the society at nominal charges.
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="col-sm-8 pt-20">
+            <h3>Our Vision</h3>
+            <div>
+                Strengthen the development of associated learning centre as a proactive role model for high quality and
+                learner centric computer literacy programme & be the most admired institute among the nation's premier
+                institute for examination and certification in the field of Information Technology Education &
+                Development
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <img class="img-fluid img-thumbnail" src="/e3da03f6-1664-4b5c-93af-d3b33f68e613.jfif" />
+        </div>
+    </div>
+
+</div>
+
+
 <?php include "include/footer.php"; ?>
--- mission-vision.php
+++ mission-vision.php
@@ -1,39 +1,77 @@
 <?php include "include/header.php"; ?>
-<!------mission & vission---->
-<div class="mv">
-    <div class="miss">
-    <img src="assets/images/mission.jpg" alt="">
+
+
+<div class="achivements" style="background: url(assets/images/slide/breadcrumb.jpg);padding: 50px;">
+    <div class="sec-title mb-3 text-center">
+        <h2 class="mb-0" style="color:#fff;">MISSION & VISION</h2>
+        <p style="color:#fff;">Home / Mission & Vision</p>
     </div>
-    <h1>Mission & Vission</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>
-       Dolor enim perspiciatis ipsam rerum architecto adipisci<br>
-       Quo suscipit modi doloribus magnam quibusdam ex assumenda,<br>
-       Quo suscipit modi doloribus magnam quibusdam ex assumenda,<br>
-       vel perferendis a. Vero a esse error voluptate, praesentium <br>
-       Reiciendis quibusdam exercitationem cum aliquid aut eligendi<br>
-       Et fuga repudiandae voluptatem nesciunt labore veniam ipsam est<br>
-       Similique eaque dolore quis nostrum accusantium. Facilis quibusdam<br>
-       Porro illum ipsa dolor sequi corporis quisquam vero, iste nostrum <br>
-       Assumenda. Aut incidunt sequi, a eaque qui culpa delectus doloribus<br>
-       Est autem eum repellat repellendus, non iste enim vitae nemo veritatisc<br>
-       Eius quos excepturi illum, voluptatem magni tenetur unde ad! Quasi id sit impedit!</p>
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
 </div>
-<div class="p2">
-    <div class="miss2">
-    <img src="assets/images/our vission.jpeg" alt="">
+
+<div class="container content">
+    <div class="row">
+
+        <div class="col-sm-8">
+            <h3>Mission & Vision</h3>
+            <div>
+                Sanjay Gandhi Computer Saksharta Mission Trust has been registered under the Public Trust Act 1882,
+                (Reg.No.2327) from Govt. Of India N.C.T., Delhi, working in different fields of Programme & Commercial
+                Training Organization is also certified by ISO 9001: 2015 Org. Through the Govt. is taking major steps
+                in influencing the computer education in the country but the participation of NGO's in spreading this
+                vital utility technical education can't be denied. After a through survey it was found that there is a
+                lot of awareness of computers even in the remotest of areas but the general public can't be equipped
+                with the art firstly due to unaffordable charges laid down by big private computer centers and also that
+                the general public have some misconception in their mind about learning computers.
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <img class="img-fluid img-thumbnail" src="/c04e1242-7cfa-4a99-8123-f80511541b3a.jfif" />
+        </div>
+
     </div>
-<h2>Vission</h2>
-<h6>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>
-       Dolor enim perspiciatis ipsam rerum architecto adipisci<br>
-       Quo suscipit modi doloribus magnam quibusdam ex assumenda,<br>
-       Quo suscipit modi doloribus magnam quibusdam ex assumenda,<br>
-       vel perferendis a. Vero a esse error voluptate, praesentium <br>
-       Reiciendis quibusdam exercitationem cum aliquid aut eligendi<br>
-       Et fuga repudiandae voluptatem nesciunt labore veniam ipsam est<br>
-       Similique eaque dolore quis nostrum accusantium. Facilis quibusdam<br>
-       Porro illum ipsa dolor sequi corporis quisquam vero, iste nostrum <br>
-       Assumenda. Aut incidunt sequi, a eaque qui culpa delectus doloribus<br>
-       Est autem eum repellat repellendus, non iste enim vitae nemo veritatisc<br>
-       Eius quos excepturi illum, voluptatem magni tenetur unde ad! Quasi id sit impedit!</h4>
-       </div>
+
+
+    <div class="row">
+
+        <div class="col-sm-4">
+            <img class="img-fluid img-thumbnail" src="/dbda31b8-1d68-470f-b55d-b3729ab77b44.jfif" />
+        </div>
+        <div class="col-sm-8">
+            <h3>Our Mission</h3>
+            <div>
+                Today, the computer is indispensable, and its presence has become very important and necessary in our
+                daily life, and it has become easier for us to do many operations and activities.some big Institution
+                run their computer programme in higher charges. Due to which the middle class families of our societies
+                cannot afford the load of their charges. Our aim is to make everyone Computer Literacy to every nook and
+                corner of the society at nominal charges.
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="col-sm-8 pt-20">
+            <h3>Our Vision</h3>
+            <div>
+                Strengthen the development of associated learning centre as a proactive role model for high quality and
+                learner centric computer literacy programme & be the most admired institute among the nation's premier
+                institute for examination and certification in the field of Information Technology Education &
+                Development
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <img class="img-fluid img-thumbnail" src="/e3da03f6-1664-4b5c-93af-d3b33f68e613.jfif" />
+        </div>
+    </div>
+
+</div>
+
+
 <?php include "include/footer.php"; ?>
