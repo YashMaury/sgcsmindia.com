a1b3d011725773d3150528ae57a729ab8b942369
YashMaury <yasmaurya2000@gmail.com>
2023-09-06 07:27:13 +0530

    upd

--- include/header.php
+++ include/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
 
-    <link href="https://sgcsmindia.org/templates/assets/style.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/about.css">
